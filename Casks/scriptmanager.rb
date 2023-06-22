6f8611daeaeb63c719a1388cb7ce21edcfdc6c2d
Daniel Filler <Daniel.filler@dm.de>
2023-06-22 12:46:48 +0200

    Add scriptmanager-cask

---
+++ Casks/scriptmanager.rb
@@ -0,0 +1,11 @@
+cask "scriptmanager" do
+  version "1.0.0"
+  sha256 "324a85b914244641ba7e57ed128411e5a43078d262bb8f3d1c871ca1df6ee267"
+
+  url "https://github.com/DanielFiller30/ScriptManager/archive/refs/tags/#{version}.tar.gz"
+  name "ScriptManager"
+  desc "An easy and comfortable menu bar-tool, to organize and use your own terminal-scripts."
+  homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.0"
+
+  app "ScriptManager.app"  
+end
