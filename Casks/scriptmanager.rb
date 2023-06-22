7302f9f58d577b8f8c5133d698b1139d883a6a3c
Daniel Filler <Daniel.filler@dm.de>
2023-06-22 13:05:52 +0200

    v1.0.1

--- Casks/scriptmanager.rb
+++ Casks/scriptmanager.rb
@@ -1,11 +1,11 @@
 cask "scriptmanager" do
-  version "1.0.0"
-  sha256 "324a85b914244641ba7e57ed128411e5a43078d262bb8f3d1c871ca1df6ee267"
+  version "1.0.1"
+  sha256 "e8b0ee5cbfcc47c6619aba5c0bb24b4529d8b23f6a5aab718f291ae7d15fbc53"
 
   url "https://github.com/DanielFiller30/ScriptManager/archive/refs/tags/#{version}.tar.gz"
   name "ScriptManager"
   desc "An easy and comfortable menu bar-tool, to organize and use your own terminal-scripts."
-  homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.0"
+  homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.1"
 
   app "ScriptManager.app"  
 end
