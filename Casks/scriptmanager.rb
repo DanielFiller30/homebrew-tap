758ed23f949d8dafeb12fcea0f6a902bb9457b3f
Filler, Daniel <Daniel.filler@dm.de>
2024-07-23 12:37:17 +0200

    fix spell check

--- Casks/scriptmanager.rb
+++ Casks/scriptmanager.rb
@@ -2,7 +2,7 @@ cask "scriptmanager" do
   version "1.0.3"
   sha256 "6367d803edb65480567bd2494b21c5a6e0890a6f1e2449780123365b667a7df7"
 
-  url "https://github.com/DanielFiller30/ScriptManager/releases/download/#{version}/ScriptManager.zip"
+  url "https://github.com/DanielFiller30/ScriptManager/releases/download/1.0.3/ScriptManager.zip"
   name "ScriptManager"
   desc "Manager to organize and use your own terminal-scripts"
   homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.3"
