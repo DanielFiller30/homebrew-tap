8fc48b15b05755bdc6d49857f3d6e7881dda4548
Filler, Daniel <Daniel.filler@dm.de>
2024-07-29 08:54:05 +0200

    V4.1: fix settings

--- Casks/scriptmanager.rb
+++ Casks/scriptmanager.rb
@@ -1,11 +1,11 @@
 cask "scriptmanager" do
-  version "1.0.3"
-  sha256 "6367d803edb65480567bd2494b21c5a6e0890a6f1e2449780123365b667a7df7"
+  version "1.0.4"
+  sha256 "4774a85aac494c7593b4b076af7fdb34001c112af04c588976b336fc55db0c0f"
 
   url "https://github.com/DanielFiller30/ScriptManager/releases/download/#{version}/ScriptManager.zip"
   name "ScriptManager"
   desc "Manager to organize and use your own terminal scripts"
-  homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.3"
+  homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.4"
 
   app "ScriptManager.app"
 end
