d1c9c7f6e34a018cb7bfae961fb74a9b7f349704
Filler, Daniel <Daniel.filler@dm.de>
2024-07-30 20:06:38 +0200

    V4.2: bug fixing

--- Casks/scriptmanager.rb
+++ Casks/scriptmanager.rb
@@ -1,11 +1,11 @@
 cask "scriptmanager" do
-  version "1.0.4"
-  sha256 "4774a85aac494c7593b4b076af7fdb34001c112af04c588976b336fc55db0c0f"
+  version "1.0.5"
+  sha256 "f253060196cadd7d336d427d5bb87f4bf123fb04ec18f9dcdc564aae67749802"
 
   url "https://github.com/DanielFiller30/ScriptManager/releases/download/#{version}/ScriptManager.zip"
   name "ScriptManager"
   desc "Manager to organize and use your own terminal scripts"
-  homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.4"
+  homepage "https://github.com/DanielFiller30/ScriptManager/tree/1.0.5"
 
   app "ScriptManager.app"
 end
